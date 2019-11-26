039a01cd8524d44d324a3b02d32f21492358685a
Christian Matthe <christian.matthe.ger@gmail.com>
2019-11-26 14:11:01 +0100

    setup_env.py currently broken

--- setup_env.py
+++ setup_env.py
@@ -8,13 +8,16 @@ env_name = "test_env" # ask for this in the beginning
 #TODO Use requirements.txt or environment.yml for smoother setup
 #os.system("python3 optimize_chunk_3.1.2.py")
 
+''' Just tell user to create and activte environment first
 os.system("conda create --name {0}".format(env_name))
-os.system("conda install --name {0} os".format(env_name))
-os.system("conda activate {0}".format(env_name))
+os.system("conda init bash")# initialize shell to allow conda activate. Currently assumes bash is used by os.system.
+'''
+#os.system("conda install --name {0} os".format(env_name))# unnecessary os comes with python?
+#os.system("conda activate {0}".format(env_name))
 
 #Install wobble
-os.system("conda install tensorflow")
-os.system("python3 wobble_19_03_2019/wobble/setup.py develop")
+os.system("conda install tensorflow=1.12.0")
+os.system("python3 wobble_19_03_2019/wobble/setup.py develop") #setup.py doesn't work if caled from top repository
 #Install additional required packages
 os.system("conda install numpy")
 os.system("conda install scipy")
