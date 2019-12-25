31f1e9e610dc0b1552449e4a0f12f0fa23dbfa97
christianmatthe <christian.matthe.ger@gmail.com>
2019-12-25 13:58:53 +0100

    work on process_results.py

--- wobble_aux/process_results.py
+++ wobble_aux/process_results.py
@@ -99,6 +99,7 @@ def make_vels(vels_dir, results_file, serval_dir, carmenes_object_ID, starname, 
     array = np.ndarray.transpose(array)
     np.savetxt(vels_dir + starname + "_serval_avcn" + '.vels', array, fmt ='%.18f')
     
+
 #plan: make standart results import function (wobble-Serval cross match)
 class Results_ws():
     """
@@ -115,79 +116,101 @@ class Results_ws():
     def __init__(self
                  , wobble_file
                  , serval_dir
+                 , carmenes_object_ID
                  , bary_starname
                  , load_bary = True
                  , archive = True
-                 , bary_archive = os.path.dirname(__file__) + "/" + "../results/bary_archive/"
-                 )
-        self.bary_archive = bary_archive
+                 , bary_archive = os.path.dirname(os.path.abspath(__file__)) + "/" + "../results/bary_archive/"
+                 , correct_w_for_drift = False
+                 , correct_drift = True
+                 , correct_NZP = True
+                 , correct_SA = True
+                 ):
+        #if bary_archive == "/" + "../results/bary_archive/":
+            #file_dir = os.path.dirname(os.path.abspath(__file__))
+            #print(file_dir )
+            #bary_archive = file_dir + bary_archive
         os.makedirs(bary_archive, exist_ok = True)
-        self.bary_starname = bary_starname
         # Import wobble
-        self.w_file_basename, = os.path.splitext(os.path.basename(wobble_file))
-        self.wobble_res = h5py.(wobble_file, 'r')
-        self.w_dates = wobble_res['dates'][()] 
-        self.w_dates_utc = wobble_res['dates_utc'][()]
-        self.w_orders = wobble_res['orders'][()]
-        self.w_epochs = wobble_res['epochs'][()]
-        self.w_RVs = wobble_res['star_time_rvs'][()]
-        self.w_RVs_er = wobble_res['star_time_sigmas'][()]
+        wobble_res = h5py.File(wobble_file, 'r')
+        w_dates = wobble_res['dates'][()] 
+        w_dates_utc = wobble_res['dates_utc'][()]
+        w_orders = wobble_res['orders'][()]
+        w_epochs = wobble_res['epochs'][()]
+        w_RVs = wobble_res['star_time_rvs'][()]
+        w_RVs_er = wobble_res['star_time_sigmas'][()]
         #orderwise RVs 
-        N_ord = len(self.w_orders) #should be equivalent to ["R"]
+        N_ord = len(w_orders) #should be equivalent to ["R"]
         k=np.arange(N_ord)
         keys_orderRVs=[]
         for j in k:
             keys_orderRVs.append('order'+str(j))
         #initialize arrays
-        self.w_order_RVs = np.zeros((N_ord,len(w_dates)))
-        self.w_order_RVs_ivars = np.zeros((N_ord,len(w_dates)))
-        self.w_order_RV_scatter = np.zeros(len(w_dates))
+        w_order_RVs = np.zeros((N_ord,len(w_dates)))
+        w_order_RVs_ivars = np.zeros((N_ord,len(w_dates)))
+        w_order_RV_scatter = np.zeros(len(w_dates))
         for j, k in enumerate(keys_orderRVs):
             temp = wobble_res[k] #load order dataset
             tempRVs = temp['star_rvs'][()] # load RVs for this order at all epochs
             tempRVs_ivars = temp['star_ivars_rvs'][()] # load error of RVs
-            self.w_order_RVs[j,:] = tempRVs # fill values into Rows of array
-            self.w_order_RVs_ivars[j,:] = tempRVs_ivars
+            w_order_RVs[j,:] = tempRVs # fill values into Rows of array
+            w_order_RVs_ivars[j,:] = tempRVs_ivars
             if np.any(tempRVs_ivars < 0.0)== True:
                 print("Found negative ivars.")
         #we may want to drop  these orders as they may represent *maxima* in optimization
         for j in range (len(w_dates)):
-            self.w_order_RV_scatter[j] = np.nanstd(w_order_RVs[:,j])
+            w_order_RV_scatter[j] = np.nanstd(w_order_RVs[:,j])
         
         #barycentric correction
         #load barycorrected rvs from pickle archive to save time on recalculting them
-        filename_RVs_barycorr = self.bary_archive + self.w_file_basename +"_barycorr.pkl"
-        filename_order_RVs_barycorr = self.bary_archive + self.w_file_basename +"_barycorr_orders.pkl"
+        # TODO Isssue in next line
+        w_file_basename, = os.path.splitext(os.path.basename(wobble_file))
+        filename_RVs_barycorr = bary_archive + w_file_basename +"_barycorr.pkl"
+        filename_order_RVs_barycorr = bary_archive + w_file_basename +"_barycorr_orders.pkl"
         if load_bary:
             try:
                 with open(filename_RVs_barycorr, "rb") as f:
-                    self.w_RVs_barycorr = pickle.load(f)
+                    w_RVs_barycorr = pickle.load(f)
                 with open(filename_order_RVs_barycorr, "rb") as f:
-                    self.w_order_RVs_barycorr = pickle.load(f)
+                    w_order_RVs_barycorr = pickle.load(f)
             except FileNotFoundError:
                 print("RV file not found, recalculating barycentric corrections")
                 load_bary = False
         if not load_rvs:
-            self.w_RVs_barycorr = np.zeros(len(self.w_dates))
+            w_RVs_barycorr = np.zeros(len(w_dates))
             for n in tqdm(range(len(w_RVs_barycorr))):
-                self.w_RVs_barycorr[n]=bary.get_BC_vel(self.w_dates_utc[n], starname= self.bary_starname, lat=_lat, longi=_lon, alt=_elevation, zmeas=self.w_RVs[n]/lightvel,
+                w_RVs_barycorr[n] = bary.get_BC_vel(w_dates_utc[n], starname = bary_starname, lat=_lat, longi=_lon, alt=_elevation, zmeas = w_RVs[n]/lightvel,
                                                         leap_update = False #HACK barycorrpy issue 27
                                                         )[0]
                 
-            self.w_order_RVs_barycorr = self.w_order_RVs
-            for order in tqdm(range(len(self.w_order_RVs))):
-                for ep in tqdm(range(len(self.w_order_RVs[order]))):
-                    self.w_order_RVs_barycorr[order, ep] = bary.get_BC_vel(self.w_dates_utc[ep], starname=self.bary_starname, lat=_lat, longi=_lon, alt=_elevation, zmeas= self.w_order_RVs[order, ep]/lightvel,
+            w_order_RVs_barycorr = w_order_RVs
+            for order in tqdm(range(len(w_order_RVs))):
+                for ep in tqdm(range(len(w_order_RVs[order]))):
+                    w_order_RVs_barycorr[order, ep] = bary.get_BC_vel(w_dates_utc[ep], starname=bary_starname, lat=_lat, longi=_lon, alt=_elevation, zmeas= w_order_RVs[order, ep]/lightvel,
                                                         leap_update = False #HACK barycorrpy issue 27
                                                         )[0]
             if archive:
                 with open(filename_RVs_barycorr) as f:
-                    pickle.dump(self.w_RVs_barycorr, f)
+                    pickle.dump(w_RVs_barycorr, f)
                 with open(filename_order_RVs_barycorr) as f:
-                    pickle.dump(self.w_order_RVs_barycorr, f)
+                    pickle.dump(w_order_RVs_barycorr, f)
             
-        # Import SERVAL
-        ser_avcn = np.loadtxt(serval_file)
+        # Import SERVAL TODO make some of these that aren't strictly necessary for corrections optional
+        # TODO replace [i] with carmenes_object_ID
+        ser_avcn = np.loadtxt(servaldir+ carmenes_object_ID +"/"+ carmenes_object_ID +".avcn.dat")
+        #read in also info file, from which we get SNR and airmass
+        ser_info = np.genfromtxt(servaldir+ carmenes_object_ID +"/"+ carmenes_object_ID +".info.cvs", delimiter=";")
+        ser_addinfo=np.zeros((len(ser_avcn), 2)) #initializes array
+        for n in range(len(ser_avcn)):
+            ind_jd = np.where(np.abs(ser_info[:,1]-ser_avcn[n,0]) == np.nanmin(np.abs(ser_info[:,1]-ser_avcn[n,0])))[0][0] #ser_info [:,1] is BJD and so is ser_rvc[n,0] this matches the ones closest to each other
+            ser_addinfo[n,0] = ser_info[ind_jd, 3] # SNR
+            ser_addinfo[n,1] = ser_info[ind_jd, 8] # Airmass
+        #Import serval orderwise Rvs
+        ser_rvo = np.loadtxt(servaldir+ carmenes_object_ID +"/"+ carmenes_object_ID +".rvo.dat")
+        ser_rvo = ser_rvo[ind_finitedrift]
+        
+        ser_rvo_err = np.loadtxt(servaldir+ carmenes_object_ID +"/"+ carmenes_object_ID +".rvo.daterr")
+        ser_rvo_err = ser_rvo_err[ind_finitedrift]
         # remove entries with nan in drift
         ind_finitedrift = np.isfinite(ser_avcn[:,3])
         ser_avcn = ser_avcn[ind_finitedrift]
@@ -200,17 +223,74 @@ class Results_ws():
             if (ser_avcn[ind_jd,0]-w_dates[n])*24*60<20.: #only takes matches closer than 20 minutes
                 indices_serval.append(ind_jd)
                 indices_wobble.append(n)
-        #TODO finish this
+                
+        #apply to all arrays and write to object
+        self.ser_avcn = ser_avcn[indices_serval]
+        self.ser_rvo = ser_rvo[indices_serval]
+        self.ser_rvo_err = ser_rvo_err[indices_serval]
+        self.ser_addinfo = ser_addinfo[indices_serval]
+        #wobble
+        self.w_dates = w_dates[indices_wobble]
+        self.w_dates_utc = w_dates_utc[indices_wobble]
+        self.w_epochs = w_epochs[indices_wobble]
+        self.w_RVs_barycorr = w_RVs_barycorr[indices_wobble]                         
+        self.w_RVs = w_RVs[indices_wobble]                                           
+        self.w_RVs_er = w_RVs_er[indices_wobble]
+        self.w_RVs_own = w_RVs_own[indices_wobble]                                   
+        self.w_RVs_unweighted = w_RVs_unweighted[indices_wobble]                     
+        self.w_bervs = w_bervs[indices_wobble]
+        self.w_order_RVs = w_order_RVs[:,indices_wobble]                  
+        self.w_order_RV_scatter = w_order_RV_scatter[indices_wobble]
+        
+        '''
+        # TODO finish this
+            - test with rewritten make_vels
+            
+        '''
         
         
-        
-        
-     #TODO   
-    def apply_corrections():
+    def apply_corrections(self, correct_w_for_drift = False, correct_drift = True, correct_NZP = True, correct_SA = True):
         #8 sa drift, 3 drift, 9 NZP
         ser_corr = - ser_avcn[:,8] - ser_avcn[:,3] - ser_avcn[:,9] # use with avcn
         ser_corr_wob = ser_corr
         #optionally remove drift correction if this has been performed during data file generation
         if correct_w_for_drift:
             ser_corr_wob = ser_corr + ser_rvc[:,3]
+            
+        ser_corr = np.zeros(len(self.ser_avcn))
+        if correct_drift: #3 drift
+            ser_corr += - ser_avcn[:,3]
+        if correct_SA:    #8 SA 
+            ser_corr += - ser_avcn[:,8]
+        if correct_NZP:   #9 NZP
+            ser_corr += - ser_avcn[:,9]
+        ser_corr_wob = ser_corr
+        if not correct_w_for_drift: # in case wobble was already drift corrected in make_data
+            ser_corr_wob += ser_avcn[:,3]
+        #apply to object
+        self.ser_corr = ser_corr
+        self.ser_corr_wob = ser_corr_wob
+        
+        #correct ser_rvo
+        for j in range(len(self.ser_rvo)):
+            self.ser_rvo[j,5:] = self.ser_rvo[j,5:] + ser_corr[j]
+            
+        # TODO error propagation
+        self.w_RVs_barycorr += ser_corr_wob
+        self.w_RVs += ser_corr_wob
+        self.w_order_RVs += ser_corr_wob
+
+if __name__ == "__main__":
+    #test
+    carmenes_object_ID = "J11417+427"
+    bary_starname = "GJ1148"
+    wobble_file = "/data/cmatthe/compare_wobble_serval/wobbledir/results_GJ1148_Kstar0_Kt3_recheck_all_orders.hdf5"
+    serval_dir = "/data/cmatthe/compare_wobble_serval/servaldir/CARM_VIS/"
+    res = Results_ws(wobble_file
+                 , serval_dir
+                 , carmenes_object_ID
+                 , bary_starname
+                 , load_bary = True
+                 , archive = True)
+    print(res.w_RVs_barycorr)
     
